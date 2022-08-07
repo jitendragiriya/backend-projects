262b2bfb636820788b2883d6ea7aa7920b3252ff
Jitendra <jitendragiriya@gmail.com>
2022-08-07 13:01:54 +0530

    allow cors all origins

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: "",
+    origin: "*",
     credentials: true,
     optionSuccessStatus: 200,
   })
